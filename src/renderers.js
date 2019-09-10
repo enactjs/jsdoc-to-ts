3ddc78f549cbbc5bca59011f37017e7ca35280fa
Ryan Duffy <ryan.duffy@lge.com>
2019-09-10 16:02:52 -0700

    fix bad function entries (i hope)

--- src/renderers.js
+++ src/renderers.js
@@ -151,9 +151,8 @@ function formatParameters (parameters, placeholders, typeRenderer) {
 	return `(${output})`;
 }
 
-const formatFunction = (section, exp, instance, name, templates, params, ret) => {
-	return renderDescription(section) +
-			(exp ? 'export ' : '') +
+const formatFunction = (exp, instance, name, templates, params, ret) => {
+	return (exp ? 'export ' : '') +
 			(instance ? '' : 'function ') +
 			`${name}${templates}${params}: ${ret};`;
 }
@@ -172,21 +171,22 @@ function defaultFunctionRenderer ({section, export: exp = false, instance = fals
 	}
 
 	const templates = placeholders.length === 0 ? '' : `<${placeholders.join(', ')}>`;
+	const desc = renderDescription(section);
 
 	if (section.tags.find(t => t.title === 'curried')) {
-		return permuteParameters(parameters).map(params => {
+		return desc +permuteParameters(parameters).map(params => {
 			const first = formatParameters(params[0], placeholders, typeRenderer);
 			if (params.length === 1) {
-				return formatFunction(section.name, templates, first, returns);
+				return formatFunction(exp, instance, section.name, templates, first, returns);
 			}
 
 			const formattedParams = params.slice(1).map(p => formatParameters(p, placeholders, typeRenderer)).join(' => ');
 			const ret = `${formattedParams} => ${returns}`;
-			return formatFunction(section, exp, instance, section.name, templates, first, ret);
+			return formatFunction(exp, instance, section.name, templates, first, ret);
 		}).join('\n');
 	}
 
-	return formatFunction(section, exp, instance, section.name, templates, parametersOutput, returns);
+	return desc + formatFunction(exp, instance, section.name, templates, parametersOutput, returns);
 }
 
 exports.defaultFunctionRenderer = defaultFunctionRenderer;
