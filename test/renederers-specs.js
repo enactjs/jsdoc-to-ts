1d287318e95f9d3688c52ca2064c561845a31102
taeyoung.hong <taeyoung.hong@lge.com>
2022-05-24 09:14:07 +0900

    WRO-2188: [React18] Update react dependencies

--- test/renederers-specs.js
+++ test/renederers-specs.js
@@ -19,42 +19,47 @@ describe('renderers', function () {
 		it('should return an object', function () {
 			const expected = 'object';
 			const actual = typeof getDefaultRenderers();
-			expect(actual).to.equal(expected);
+			expect(actual).equal(expected);
 		});
 
 		it('should allow customizing the object', function () {
 			const overrideFunc = () => {};
-			const renderer = getDefaultRenderers({module: overrideFunc});
+			const view = getDefaultRenderers({module: overrideFunc});
 
 			const expected = overrideFunc;
-			const actual = renderer.module;
-			expect(actual).to.equal(expected);
+			const actual = view.module;
+			expect(actual).equal(expected);
 		});
 	});
 
 	describe('defaultModuleRenderer', function () {
 		it('should render only header if no members', function () {
 			const renderer = () => [];
+			const view = defaultModuleRenderer({section: emptyModule, parent: emptyModule, root: emptyModule, log, renderer});
 
 			const expected = emptyModuleOutput;
-			const actual = defaultModuleRenderer({section: emptyModule, parent: emptyModule, root: emptyModule, log, renderer});
-			expect(actual).to.equal(expected);
+			const actual = view;
+			expect(actual).equal(expected);
 		});
 	});
 
 	describe('defaultHocRenderer', function () {
 		it('should a complete hoc', function () {
+			const view = defaultHocRenderer({section: completeHoc, parent: completeHoc, root: completeHoc, log});
+
 			const expected = completeHocOutput;
-			const actual = defaultHocRenderer({section: completeHoc, parent: completeHoc, root: completeHoc, log});
-			expect(actual).to.equal(expected);
+			const actual = view;
+			expect(actual).equal(expected);
 		});
 	});
 
 	describe('defaultFunctionRenderer', function () {
 		it('should render a simple function definition', function () {
+			const view = defaultFunctionRenderer({section: simpleFunction, parent: simpleFunction, root: simpleFunction, log});
+
 			const expected = simpleFunctionOutput;
-			const actual = defaultFunctionRenderer({section: simpleFunction, parent: simpleFunction, root: simpleFunction, log});
-			expect(actual).to.equal(expected);
+			const actual = view;
+			expect(actual).equal(expected);
 		});
 	});
 });
